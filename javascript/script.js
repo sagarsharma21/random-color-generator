92c1904060d8943b70c7bf8ee0955a249bee7bdc
sagarsharma21 <007sharmasagar@gmail.com>
2023-08-01 22:33:01 +0530

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+
+const getColor = () => {
+    //Generate random number
+    const random_number = Math.random();
+    
+    const valid_number = Math.floor(random_number * 16777215);
+    console.log(`The no is: `+ valid_number);
+    
+    const color_code = "#" + valid_number.toString(16);
+    console.log(`Color code is: `+ color_code);
+    
+    document.body.style.backgroundColor = color_code;
+
+}
+
+//event call / manual (using onclick)
+const a = document.getElementById("btn");  
+    console.log(`btn id is: `+ a);
+        a.addEventListener("click", getColor);
+ 
+//initial call
+getColor();
